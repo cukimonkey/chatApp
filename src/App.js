34bf802a1fde0d2a54d23b87561c06d8c2468ae3
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-15 17:39:07 +0100

    Set up the core for the chat app with React ChatEngine, add custom styling"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <ChatEngine 
+      //props:
+      height='100vh'
+      projectID='d84a1bc3-b106-4f69-8318-8eba3622ece7'
+      userName='Ursula'
+      userSecret='password1234!'
+      />
     </div>
   );
 }
