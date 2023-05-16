c4cc507128d1ca5ee15bbf317bd8452f46e49f0b
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-16 19:26:03 +0100

    Set up the ChatFeed component. PAssing down and mapping the props, setting up the basic rendering for the messages

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
 const ChatFeed = (props) => {
-console.log(props);
+
+    const {chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map ( (key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0? null : keys[index-1];
+            const isMyMessage = userName === message.sender.userName;
+
+            return(
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className='message-block'>
+                        { isMyMessage?
+                        <MyMessage message={message}/>
+                      : <TheirMessage message={message} lastMessage={message[lastMessageKey]}/>
+                        }
+                    </div>
+
+                    <div className="read-reciepts" 
+                    style={{
+                        marginRight: isMyMessage ? '18px' : '0px', 
+                        marginLeft: isMyMessage ? '0px' : '68px'
+                    }}>
+                    </div> 
+
+                </div>
+            );
+        });
+    };
 
     return(
-        <div>
-            ChatFeed
+        <div className='chat-feed'>
+            <div className="chat-title-container">
+                
+                <div className="chat-title">
+                    {chat?.title}
+                </div>
+
+                <div className="chat-subtitle">
+                    {renderMessages()}
+                    
+                    <div style = {{height: '100px'}} //to give some hight between the chats    
+                    ></div> 
+
+                    <div className="message-form-container">
+                        <MessageForm {...props} chatId={activeChat}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
