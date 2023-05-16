bbca070882ff9b59d1e01aab2a848ef4ad6fe198
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-16 20:00:49 +0100

    Set up the My Message components, check if the message is image, then display image, otherwise, display text

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+
+    if (message.attachements  && message.attachements.lenght > 0) {
+        return (
+            <img 
+                src={message.attachement[0].file}
+                alt='message-attachement' 
+                className='message-mage' 
+                style={{float: 'right'}}
+            />
+        );
+    }
+
     return (
-      <div>
-        My Message
+      <div className='message'
+      style={{float: 'right', marginRight: '18px', color: 'white', backgroundColor: 'ActiveCaption' }}>
+        {message.text}
       </div>
     )
 }
