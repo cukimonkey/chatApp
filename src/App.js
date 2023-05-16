2d3f4ee3ccb05a79baf6b25b56c0b2ac69545b7b
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-16 11:38:28 +0100

    create components for the chat feed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       projectID='d84a1bc3-b106-4f69-8318-8eba3622ece7'
       userName='Ursula'
       userSecret='password1234!'
+      renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage = {() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
     </div>
   );
