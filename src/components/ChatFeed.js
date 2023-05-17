ec677463d1cf2377122921f08a323ff08aa2004c
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-17 09:42:57 +0100

    corrected the syntax error in the renderReadReceipts function

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -9,7 +9,7 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
-    const renderReadReceipts = (messagem isMyMessage) => chat.people.map((person, index) => 
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => 
      person.last_read === message.id && (
         <div>
             key = {`read_${index}`}
